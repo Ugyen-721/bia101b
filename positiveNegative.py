8ef2bb809632ef0598a1d9856e75b3932c5fa6c4
Ugyen-721 <03230401.gcbs@rub.edu.bt>
2024-03-07 14:21:00 +0600

    check for positive and negative number

---
+++ positiveNegative.py
@@ -0,0 +1,18 @@
+# Objective:
+# Create a program that takes in user input
+# and determines if the number is positive or negative
+# print: "Your number is positive" or "Your number is negative"
+# If else
+# print()
+# input()
+
+num = float(input('Enter your Number: '))
+
+if num > 0:
+    print('The number is Positive')
+
+elif num < 0:
+    print('The number is Negative')
+
+elif num == 0:
+    print('The number is Zero')
