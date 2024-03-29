6a2c3f70c5fa5a17f41f3bd3e9e4bfa707fc3d32
Ugyen-721 <03230401.gcbs@rub.edu.bt>
2024-03-29 12:43:57 +0600

    ContainsDuplicate

---
+++ containsduplicate.py
@@ -0,0 +1,18 @@
+input_arr = [1,2,3,4,1]
+
+def containsDuplicate(nums):
+    # solution code here
+    for index1 in range(0, len(nums)):
+        for index2 in range(0, len(nums)):
+            if index1 == index2:
+                continue
+            value1 = nums[index1]
+            value2 = nums[index2]
+            if value1 == value2: # Duplicate found
+                return True
+    
+    return False
+    # either return True or False
+
+result = containsDuplicate(input_arr)
+print(result)
