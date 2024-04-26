ead2119493e51302021441b0ed96c9eb81d64747
Ugyen-721 <03230401.gcbs@rub.edu.bt>
2024-04-26 12:25:11 +0600

    Floor and Stack

--- b.py
+++ b.py
@@ -8,4 +8,26 @@ def input_str(input_string):
     print('At the end, you will be at floor number:', floor_at_end)
 
 input_str(input('Enter Brackets: '))
-    
+
+#Task 2
+# Given a set of open and close brackets
+# check to see if the input is valid
+# Example 
+# ((()))
+# output: true
+# Example ()(()) False
+
+input_str = ")()()()()()))))()()()"
+stack = []
+
+for i in input_str:
+    if i == "(":
+        stack.append(i)
+    if i == ")":
+        stack.pop()
+
+length = len(stack)
+if length != 0:
+    print('True')
+else:
+    print('False')
